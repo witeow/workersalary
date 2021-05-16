91bacf33d10a89aa96cf32d2353f58afd22315ca
lIM WI TEOW <witeow223@gmail.com>
2021-05-16 22:08:21 +0800

    extract location

--- googletoexcel.py
+++ googletoexcel.py
@@ -1,6 +1,8 @@
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
+import numpy as num
+import pandas as pd
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'google_excel_key.json'
@@ -21,8 +23,27 @@ sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="workinghours").execute()
 values = result.get('values', [])
-print(result["values"])
-for row in result["values"]:
-    print(row)
+# print(result)
+
+# print(len(values)) 
+
+# creating a dictionary with location as key and name as value
+# for example:
+# 55_lentor_way = {"Name" : [Islam, Subrot]
+# "1" : [8,4,8,0]
+# "2" : [8,2,8,3]}
+###################################################################################
+# creating different locations for different sheets
+locations = []
+for row in values[1:]:
+        try:
+                if row[3] not in locations:
+                        locations.append(row[3])
+                if row[5] not in locations:
+                        locations.append(row[5])
+        except IndexError:
+                continue
+
+print(locations)
 
 
